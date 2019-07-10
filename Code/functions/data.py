a6f641659ea08f640769c1ec66f4e29b79671e87
pouple <antonin@verdier.fr>
2019-07-10 15:16:56 +0200

    Improve selection of data

--- Code/functions/data.py
+++ Code/functions/data.py
@@ -8,24 +8,38 @@ params = sett.params()
 paths = sett.paths()
 
 
-def load_data(animal, verbose):
+def load_data(animal, selection, verbose):
 	path_raw = os.path.join(paths.path2Data, animal, 'Raw Data')
 	
 	raw_paths = [os.path.join(path_raw, file) for file in ['ROIBinmaps_{0}.mat'.format(animal), 'ALLDFF.mat', 'ALLBEHmatrix.mat', 'ALLf0.mat']]
 
+	meta_df = pd.read_csv(os.path.join(path_raw, 'meta_df.csv'), index_col=0)
+
+	for param in selection:
+		print(param)
+		if selection[param] != None:
+			meta_df = meta_df[meta_df[param].isin(selection[param])]
+
+	trials_of_interest = meta_df.index.tolist()
+
+	if not trials_of_interest:
+		raise ValueError('Selection is too restrictive, please select at least 1 trial')
+
 	# Load matrices as dictionnary {name: matrix_values}
 	roi_file, raw_mat, raw_beh, f0 = list(map(loadmat, raw_paths))
 	
-	meta_df = pd.read_csv(os.path.join(path_raw, 'meta_df.csv'), index_col=0)
-
 	# Indexing data of interest
 	roi_tensor = roi_file['ROIBinmaps']
 
 	# Remove the first second of acquisition
 	acti = raw_mat['ALLDFF'][:,15:,:]
+	acti = acti[:,:,trials_of_interest]
 
 	beh_mat = raw_beh['ALLBEHmatrix']
+	beh_mat = beh_mat[trials_of_interest]
+	
 	f0 = f0['F0ALL']
+	f0 = f0[:,trials_of_interest]
 
 	# Check # of trial consistency
 	assert acti.shape[2] == beh_mat.shape[0]
@@ -36,7 +50,7 @@ def load_data(animal, verbose):
 	N, T, K = acti.shape
 	if verbose: print('Data contain activity of', N, 'ROI, on a time scale of', T/15, 'seconds, during', K, 'trials')
 
-	return meta_df, roi_tensor, acti, beh_mat, f0
+	return meta_df, roi_tensor, acti, beh_mat, f0, trials_of_interest
 
 def save_data(interpolated_acti, flag_roi, drop_trial, roi_tensor, color_df, animal):
 	# assert beh_mat.shape[0] + len(drop_trial) == raw_beh_mat.shape[0]
--- Code/main.py
+++ Code/main.py
@@ -26,13 +26,46 @@ parser.add_argument('--animal', '-a', type=int, default=0,
 parser.add_argument('--tol', '-tl', type=int, default=15, 
 					help='Tolerance for data')
 
+# Select relevant data for processing
+parser.add_argument('--Block', '-B', type=int, nargs='*', default=None, 
+					help='Blocks to choose from. All blocks are taken by default')
+parser.add_argument('--Odor', '-O', type=str, nargs='*', default=None, 
+					help='Odors to choose from. All odors are taken by default')
+parser.add_argument('--Stimulus', '-S', type=str, nargs='*', default=None, 
+					help='Stimulus to choose from. All stimulus are taken by default')
+parser.add_argument('--Behavior', '-BE', type=str, nargs='*', default=None, 
+					help='Behaviors to choose from. All behaviors are taken by default')
+parser.add_argument('--Reward', '-R', type=int, default=None, 
+					help='Rewards to choose from. All rewards are taken by default')
+parser.add_argument('--Date', '-D', type=str, nargs='*', default=None, 
+					help='Dates to choose from. All dates are taken by default')
+parser.add_argument('--Day', '-DY', type=int, nargs='*', default=None, 
+					help='Days to choose from. All days are taken by default')
+parser.add_argument('--Learnstate', '-L', type=int, default=None, 
+					help='Learning states to choose from. All learning states are taken by default')
+parser.add_argument('--Expclass', '-E', type=int, nargs='*', default=None, 
+					help='Experiment classes to choose from. All experiment classes are taken by default')
+parser.add_argument('--Performance', '-P', type=int, nargs='*', default=None, 
+					help='Performances to choose from. All performances are taken by default')
+
 args = parser.parse_args()
 
-# Choose an animal to process
-animal = params.animal_list[args.animal]
+selection = {
+	'Block':args.Block, 
+	'Odor': args.Odor,
+	'Stimulus': args.Stimulus,
+	'Behavior': args.Behavior,
+	'Reward': args.Reward,
+	'Date': args.Date,
+	'Day': args.Day,
+	'Learning State': args.Learnstate,
+	'Experiment Class': args.Expclass,
+	'Performance': args.Performance
+}
 
 # Loading data from folder
-meta_df, roi_tensor, acti, beh_mat, f0 = data.load_data(animal, args.verbose)
+meta_df, roi_tensor, acti, beh_mat, f0, trials_of_interest = data.load_data(params.animal_list[args.animal], 
+																			selection, args.verbose)
 
 if args.plotting: plot.map_of_rois(acti, roi_tensor)
 
@@ -77,5 +110,5 @@ color_df = data.generate_color_df(beh_mat)
 # if args.plotting: plot.behaviorgram(raw_beh, trials_to_drop)
 
 # Save processed data in a folder
-data.save_data(interpolated_acti, flag_roi, trials_to_drop, roi_tensor, color_df, animal)
+data.save_data(interpolated_acti, flag_roi, trials_to_drop, roi_tensor, color_df, params.animal_list[args.animal])
 
