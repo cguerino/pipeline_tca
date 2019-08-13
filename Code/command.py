3fb3e54d1bec7aa0b15780c55197ca6b139930cd
pouple <antonin@verdier.fr>
2019-08-13 13:57:09 +0200

    remove files

--- Code/command.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for animal in range(15):
-	for e in [1, 2, 3]:
-		for f in ['non_negative_parafac', 'parafac', 'custom_parafac']:
-			os.system('X:\\Antonin\\Pipeline\\Code\\TCA.py -a {} -E {} -f {}'.format(animal, e, f))
-			os.system('X:\\Antonin\\Pipeline\\Code\\meta_analysis.py -a {} -E {} -f {} -ts "acti" -corr'.format(animal, e, f))
